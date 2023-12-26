7665b0629780442eacd470d17d52725ece07c246
Lhamjav Sanjmyatav <lkhamjavsanjmytav@gmail.com>
2023-12-26 16:16:26 +0800

    style ynzlsan

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -11,21 +11,21 @@ export const Blog = () => {
     return (
         <div className='bg-white flex justify-center items-center'>
             <div className='flex justify-between  w-3/4 lg:w-[1216px] flex-col'>
-                <h1>All Blog Post</h1>
+                <h1 className='text-2xl font-bold py-12'>All Blog Post</h1>
                 <div className='grid grid-cols-3 gap-5'>
                     {articles.map((article) => {
                         return (
-                            <div>
-                                <img src={article.cover_image ? article.cover_image : ("https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg")} alt="" />
-                                <div className='flex py-1 px-2.5 text-[#4B6BFB] bg-[#4B6BFB0D] rounded-md max-w-24 justify-center'>
+                            <div className='border rounded-xl p-4 flex  justify-between flex-col'>
+                                <img className='rounded-xl h-60 w-[360px] mb-4' src={article.cover_image ? article.cover_image : ("https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg")} alt="" />
+                                <div className='flex py-1 px-2.5 text-[#4B6BFB] bg-[#4B6BFB0D] rounded-md max-w-24 justify-center '>
                                     {/* {props.title} */}
                                     Technology
                                 </div>
                                 {/* asjhdkjas */}
-                                <h2> {article.title}</h2>
-                                <div>
-                                    <img src={article.user.profile_image ? article.user.profile_image : ("https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif")} alt="" />
-                                    <p className='name'>{article.user.name}</p>
+                                <h2 className='pt-4 pb-5'> {article.title}</h2>
+                                <div className='flex  items-center'>
+                                    <img className='w-9 h-9 rounded-full ' src={article.user.profile_image ? article.user.profile_image : ("https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif")} alt="" />
+                                    <p className='pl-3 pr-5'>{article.user.name}</p>
                                     <p className='onsar'>{article.readable_publish_date} 2022</p>
                                 </div>
                             </div>
@@ -33,7 +33,8 @@ export const Blog = () => {
                         )
                     })}
                 </div>
-                <button>load more</button>
+                <div className='flex justify-center items-center'>                <button className='py-3 px-5 border rounded-xl mt-8 w-[123px] '>Load more</button></div>
+
             </div>
         </div>
     )
