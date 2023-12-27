f0ab890bbd7275e5500b7db8d5ffa1c49b9f2edb
Lhamjav Sanjmyatav <lkhamjavsanjmytav@gmail.com>
2023-12-27 19:33:51 +0800

    article id damjuulj ogdog bolson

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Api } from './Api'
 import { Wbutton } from './Wbutton'
+import Link from 'next/link'
 export const Blog = () => {
     const [articles, setArticles] = useState([])
     useEffect(() => {
@@ -15,19 +16,21 @@ export const Blog = () => {
                 <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
                     {articles.map((article) => {
                         return (
-                            <div className='border rounded-xl p-4 flex  justify-between flex-col'>
-                                <img className='rounded-xl h-60 w-[360px] mb-4' src={article.cover_image ? article.cover_image : ("https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg")} alt="" />
-                                <div className='flex py-1 px-2.5 text-[#4B6BFB] bg-[#4B6BFB0D] rounded-md max-w-24 justify-center '>
-                                    {/* {props.title} */}
-                                    Technology
+                            <Link href={`/${article.id}`}>
+                                <div className='border rounded-xl p-4 flex  justify-between flex-col'>
+                                    <img className='rounded-xl h-60 w-[360px] mb-4' src={article.cover_image ? article.cover_image : ("https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg")} alt="" />
+                                    <div className='flex py-1 px-2.5 text-[#4B6BFB] bg-[#4B6BFB0D] rounded-md max-w-24 justify-center '>
+                                        {/* {props.title} */}
+                                        Technology
+                                    </div>
+                                    <h2 className='pt-4 pb-5 text-2xl font-semibold'> {article.title}</h2>
+                                    <div className='flex  items-center'>
+                                        <img className='w-9 h-9 rounded-full ' src={article.user.profile_image ? article.user.profile_image : ("https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif")} alt="" />
+                                        <p className='pl-3 pr-5 font-normal text-base text-[#97989F]'>{article.user.name}</p>
+                                        <p className='font-normal text-base text-[#97989F]'>{article.readable_publish_date} 2022</p>
+                                    </div>
                                 </div>
-                                <h2 className='pt-4 pb-5 text-2xl font-semibold'> {article.title}</h2>
-                                <div className='flex  items-center'>
-                                    <img className='w-9 h-9 rounded-full ' src={article.user.profile_image ? article.user.profile_image : ("https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif")} alt="" />
-                                    <p className='pl-3 pr-5 font-normal text-base text-[#97989F]'>{article.user.name}</p>
-                                    <p className='font-normal text-base text-[#97989F]'>{article.readable_publish_date} 2022</p>
-                                </div>
-                            </div>
+                            </Link>
                         )
                     })}
                 </div>
