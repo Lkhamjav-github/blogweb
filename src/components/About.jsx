c9868c8e8f2eee2c8b50bb969217204ab407b35f
Lhamjav Sanjmyatav <lkhamjavsanjmytav@gmail.com>
2024-03-12 20:52:09 +0800

    web soligdoh uil ywc done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,28 +10,40 @@ export const About = () => {
             .then(data => setArticles(data))
     }, [])
     function nextClick() {
-        setIndex(index + 1);
-    }
-    function backClick() {
-        setIndex(index - 1);
+        if (index == 30) {
+            setIndex(1);
+        }
+        else {
+            setIndex(index + 1);
+        }
+
     }
 
+    function backClick() {
+        if (index != 0) {
+            setIndex(index - 1);
+        }
+    }
+
+
     let article = articles[index];
+    console.log(article)
+    console.log("index", index)
     return (
         <div className='bg-white flex justify-center items-center'>
             <div className='flex justify-between  w-3/4 lg:w-[1216px] flex-col'>
-                {/* {articles.map((article) => {
-                    return ( */}
-                <div className='border rounded-xl p-4 flex  justify-between flex-col'>
-                    <img className='relative' src={articles.cover_image ? articles.cover_image : ("./anai.png")} alt="" />
-                    <div className='bg-white absolute ml-3 mt-[335px] rounded-xl p-10'>
-                        <Button title="Technology" />
-                        <h1 className='text-4xl font-semibold w-[598px] pt-4 pb-6'>{articles.title ? articles.title : ("Grid system for better Design User Interface")}</h1>
-                        <p className='font-normal text-[#97989F]'>{articles.readable_publish_date ? articles.readable_publish_date : ("August 20, 2022")}</p>
+                <div className='grid grid-cols-1'>
+                    <div className='border rounded-xl p-4 flex  justify-between flex-col'>
+                        <img className='relative h-[583px]' src={article?.cover_image ? article?.cover_image : ("./anai.png")} />
+                        <div className='bg-white absolute ml-3 mt-[315px] rounded-xl p-10'>
+                            <Button title="Technology" />
+                            <h1 className='text-4xl font-semibold w-[598px] pt-4 pb-6'>{article?.title ? article?.title : ("Grid system for better Design User Interface")}</h1>
+
+                            <p className='font-normal text-[#97989F]'>{article?.readable_publish_date ? article?.readable_publish_date : ("August 20, 2022")}</p>
+
+                        </div>
                     </div>
                 </div>
-                {/*     )
-                 })} */}
                 <div className='flex justify-end pt-3 gap-2'>
                     <button onClick={backClick}>
                         <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
